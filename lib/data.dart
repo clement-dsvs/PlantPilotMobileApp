47dce1a02625e4f8b3a4df652741bf6de81dbd4e
PC-FIXE-HUGO\Hugo <hugotempezpro@gmail.com>
2024-04-14 23:47:11 +0200

    HT - Dev presets + forum

--- lib/data.dart
+++ lib/data.dart
@@ -12,10 +12,12 @@ class LocalData {
   var plantPilot = [
     {"id": "1234",
       "status": "active",
-      "last_message": "2000-01-01 00:00:00"},
+      "last_message": "2000-01-01 00:00:00",
+      "type": "base"},
     {"id": "4567",
       "status": "inactive",
-      "last_message": "2000-01-01 00:00:00"}
+      "last_message": "2000-01-01 00:00:00",
+      "type": "base"}
   ];
 
   var pots = [
@@ -25,7 +27,8 @@ class LocalData {
       "water_level": 100,
       "battery_level": 100,
       "plantpilot_id": "1234",
-      "last_usage": "2000-01-01 00:00:00"
+      "last_usage": "2000-01-01 00:00:00",
+      "type": "pot"
     },
     {
       "id": "234",
@@ -33,7 +36,8 @@ class LocalData {
       "water_level": 100,
       "battery_level": 100,
       "plantpilot_id": "1234",
-      "last_usage": "2000-01-01 00:00:00"
+      "last_usage": "2000-01-01 00:00:00",
+      "type": "pot"
     },
     {
       "id": "345",
@@ -41,7 +45,8 @@ class LocalData {
       "water_level": 100,
       "battery_level": 100,
       "plantpilot_id": "1234",
-      "last_usage": "2000-01-01 00:00:00"
+      "last_usage": "2000-01-01 00:00:00",
+      "type": "pot"
     },
     {
       "id": "456",
@@ -49,7 +54,8 @@ class LocalData {
       "water_level": 100,
       "battery_level": 100,
       "plantpilot_id": "1234",
-      "last_usage": "2000-01-01 00:00:00"
+      "last_usage": "2000-01-01 00:00:00",
+      "type": "pot"
     },
     {
       "id": "567",
@@ -57,7 +63,8 @@ class LocalData {
       "water_level": 100,
       "battery_level": 100,
       "plantpilot_id": "1234",
-      "last_usage": "2000-01-01 00:00:00"
+      "last_usage": "2000-01-01 00:00:00",
+      "type": "pot"
     },
     {
       "id": "678",
@@ -65,7 +72,8 @@ class LocalData {
       "water_level": 100,
       "battery_level": 100,
       "plantpilot_id": "1234",
-      "last_usage": "2000-01-01 00:00:00"
+      "last_usage": "2000-01-01 00:00:00",
+      "type": "pot"
     },
     {
       "id": "789",
@@ -73,7 +81,95 @@ class LocalData {
       "water_level": 100,
       "battery_level": 100,
       "plantpilot_id": "1234",
-      "last_usage": "2000-01-01 00:00:00"
+      "last_usage": "2000-01-01 00:00:00",
+      "type": "pot"
+    }
+  ];
+
+  var presets = [
+    {
+      "id": "123",
+      "preset_name": "preset 1",
+      "created_by": "user1",
+      "created_at": "01-01-2000 00:00:00",
+      "parameters": {
+        "water_quantity": 100,
+        "interval": 3600
+      }
+    },
+    {
+      "id": "456",
+      "preset_name": "preset 2",
+      "created_by": "user1",
+      "created_at": "01-01-2000 00:00:00",
+      "parameters": {
+        "water_quantity": 100,
+        "interval": 7200
+      }
+    }
+  ];
+
+  var topics = [
+    {
+      "id": "123",
+      "topic_name": "topic1",
+      "created_by": "user1",
+      "created_at": "01-01-2000 00:00:00",
+      "last_message_by": "user1",
+      "last_message_at": "01-01-2000 00:00:00",
+      "total_messages": 0
+    },
+    {
+      "id": "456",
+      "topic_name": "topic2",
+      "created_by": "user2",
+      "created_at": "01-01-2000 00:00:00",
+      "last_message_by": "user2",
+      "last_message_at": "01-01-2000 00:00:00",
+      "total_messages": 0
+    }
+  ];
+
+  var message = [
+    {
+      "id": "123",
+      "topic_id": "topic1",
+      "created_by": "user1",
+      "created_at": "01-01-2000 00:00:00",
+      "message": "message 1",
+      "response_to": null,
+      "modified_at": "01-01-2000 00:00:00",
+      "previous_message": null
+    },
+    {
+      "id": "234",
+      "topic_id": "topic1",
+      "created_by": "user2",
+      "created_at": "01-01-2000 00:00:00",
+      "message": "message 2",
+      "response_to": null,
+      "modified_at": "01-01-2000 00:00:00",
+      "previous_message": null
+    },
+    {
+      "id": "345",
+      "topic_id": "topic2",
+      "created_by": "user1",
+      "created_at": "01-01-2000 00:00:00",
+      "message": "message 3",
+      "response_to": null,
+      "modified_at": "01-01-2000 00:00:00",
+      "previous_message": null
+    },
+    {
+      "id": "456",
+      "topic_id": "topic2",
+      "created_by": "user2",
+      "created_at": "01-01-2000 00:00:00",
+      "message": "message 4",
+      "response_to": null,
+      "modified_at": "01-01-2000 00:00:00",
+      "previous_message": null
     }
   ];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -132,6 +132,7 @@ class _LoginPageState extends State<LoginPage> {
                   onPressed: () {
                     if (_onLoginClick(loginController.text, passwordController.text)) {
                       loginAttempt = "";
+                      Navigator.pop(context); //empéche le retour à cette vue
                       Navigator.push(context,
                           MaterialPageRoute(builder: (context) => const HomePage())); // à modifier par pushReplacement pour empecher le retour à la page de login
                     } else {
@@ -167,18 +168,34 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
   static const pageItems = [
-    {"Dashboard": Icon(Icons.home)},
-    {"Presets": Icon(Icons.precision_manufacturing)},
-    {"Forum": Icon(Icons.forum)},
-    {"Mon compte": Icon(Icons.person)}
+    {"Dashboard": {
+      "icon": Icon(Icons.home),
+      "page": HomePage()
+      }
+    },
+    {"Presets": {
+      "icon": Icon(Icons.precision_manufacturing),
+      "page": PresetsPage()
+    }
+    },
+    {"Forum": {
+      "icon": Icon(Icons.forum),
+      "page": ForumPage()
+     }
+     },
+    {"Mon compte": {
+      "icon": Icon(Icons.person),
+      "page": AccountPage()
+    }}
   ];
 
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
+    //var appState = context.watch<MyAppState>();
     List<Widget> plantPilot = [];
     List<Widget> pots = [];
     List<Widget> menuTile = [];
@@ -201,7 +218,11 @@ class HomePage extends StatelessWidget {
                     style: const TextStyle(
                         fontSize: 12,
                         fontStyle: FontStyle.italic
-                    ))
+                    )),
+                onTap: () {
+              Navigator.push(context,
+              MaterialPageRoute(builder: (context) => ItemPage(item: item)));
+              },
               )
           )
           )
@@ -221,23 +242,28 @@ class HomePage extends StatelessWidget {
               fontSize: 12,
                 fontStyle: FontStyle.italic
             ), "Niveau d'eau : ${item['water_level']}\nNiveau de batterie : ${item["battery_level"]}\nID PlantPilot : ${item["plantpilot_id"]}\nDernière action : ${item["last_usage"]!}"),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ItemPage(item: item)));
+            }
           )
       );
     }
     for (final item in pageItems) {
-      var k = item.keys.first;
-      var v = item.values.first;
+      var key = item.keys.first;
+      var icon = item.values.first["icon"];
+      Widget page = item.values.first["page"] as Widget;
       menuTile.add(ListTile(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
         tileColor: Colors.grey[300],
-        leading: v,
-        title: Text(k, textAlign: TextAlign.right),
+        leading: icon,
+        title: Text(key, textAlign: TextAlign.right),
         onTap: () {
-          // Update the state of the app
-          // Then close the drawer
-          //Navigator.pop(context);
-          print(item);
-          throw UnimplementedError();
+          if (page.runtimeType.toString() == toString()) {
+            Navigator.pop(context); //détruit la page actuel si elle est appelé
+          }
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => page));
         },
       )
       );
@@ -283,6 +309,426 @@ class HomePage extends StatelessWidget {
               ),
             )
           )
+    );
+  }
+}
+
+class ItemPage extends StatelessWidget {
+  Map<String, Object> item;
+  ItemPage({super.key, required this.item});
+  static const pageItems = [
+    {"Dashboard": {
+      "icon": Icon(Icons.home),
+      "page": HomePage()
+    }
+    },
+    {"Presets": {
+      "icon": Icon(Icons.precision_manufacturing),
+      "page": PresetsPage()
+    }
+    },
+    {"Forum": {
+      "icon": Icon(Icons.forum),
+      "page": ForumPage()
+    }
+    },
+    {"Mon compte": {
+      "icon": Icon(Icons.person),
+      "page": AccountPage()
+    }}
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> menuTile = [];
+    for (final item in pageItems) {
+      var key = item.keys.first;
+      var icon = item.values.first["icon"];
+      Widget page = item.values.first["page"] as Widget;
+      menuTile.add(ListTile(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+        tileColor: Colors.grey[300],
+        leading: icon,
+        title: Text(key, textAlign: TextAlign.right),
+        onTap: () {
+          if (page.runtimeType.toString() == toString()) {
+            Navigator.pop(context);
+          }
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => page));
+        },
+      )
+      );
+    }
+    return MaterialApp(
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+            useMaterial3: true
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('PlantPilot'),
+              centerTitle: true,
+              backgroundColor: Colors.blue,
+            ),
+            drawer: Drawer(
+              child: ListView(
+                children: <Widget>[
+                  const DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                    child: Text('Menu'),
+                  ),
+                ] + menuTile,
+              ),
+            ),
+            body: SingleChildScrollView(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    if (item["type"] == "base")
+                      ListTile(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+                        tileColor: item["status"]! == "active" ? Colors.green[300] : Colors.grey[300],
+                        leading: Icon(item["status"]! == "active" ? Icons.check_circle : Icons.close),
+                        title: Text("PlantPilot ID : ${item["id"]!}",
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            )
+                        ),
+                        subtitle: Text("Dernier message : ${item["last_message"]!}",
+                            style: const TextStyle(
+                                fontSize: 12,
+                                fontStyle: FontStyle.italic
+                            )),
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => ItemPage(item: item)));
+                        },
+                      )
+                    else if (item["type"] == "pot")
+                      ListTile(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                        tileColor: item["status"]! == "active" ? Colors.blue[100] : Colors.red[100],
+                        leading: Icon(item["status"]! == "active" ? Icons.check_circle : Icons.close),
+                        title: Text(style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold
+                        ), "Identifiant pot : ${item["id"]!}"),
+                        subtitle: Text(style: const TextStyle(
+                            fontSize: 12,
+                            fontStyle: FontStyle.italic
+                        ), "Niveau d'eau : ${item['water_level']}\nNiveau de batterie : ${item["battery_level"]}\nID PlantPilot : ${item["plantpilot_id"]}\nDernière action : ${item["last_usage"]!}"),
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => ItemPage(item: item)));
+                        }
+                    )
+                  ],
+              ),
+            )
+        )
+    );
+  }
+}
+
+class PresetsPage extends StatelessWidget {
+  const PresetsPage({super.key});
+  static const pageItems = [
+    {"Dashboard": {
+      "icon": Icon(Icons.home),
+      "page": HomePage()
+    }
+    },
+    {"Presets": {
+      "icon": Icon(Icons.precision_manufacturing),
+      "page": PresetsPage()
+    }
+    },
+    {"Forum": {
+      "icon": Icon(Icons.forum),
+      "page": ForumPage()
+    }
+    },
+    {"Mon compte": {
+      "icon": Icon(Icons.person),
+      "page": AccountPage()
+    }}
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> menuTile = [];
+    for (final item in pageItems) {
+      var key = item.keys.first;
+      var icon = item.values.first["icon"];
+      Widget page = item.values.first["page"] as Widget;
+      menuTile.add(ListTile(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+        tileColor: Colors.grey[300],
+        leading: icon,
+        title: Text(key, textAlign: TextAlign.right),
+        onTap: () {
+          if (page.runtimeType.toString() == toString()) {
+            Navigator.pop(context);
+          }
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => page));
+        },
+      )
+      );
+    }
+    return MaterialApp(
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+            useMaterial3: true
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('PlantPilot'),
+              centerTitle: true,
+              backgroundColor: Colors.blue,
+            ),
+            drawer: Drawer(
+              child: ListView(
+                children: <Widget>[
+                  const DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                    child: Text('Menu'),
+                  ),
+                ] + menuTile,
+              ),
+            ),
+            body: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  const Center(child: Row(children: [Icon(Icons.arrow_downward), Text("Mes Presets"), Icon(Icons.arrow_downward)])),
+                  for (final item in data.presets)
+                    ListTile(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                        tileColor: Colors.grey[300],
+                        title: Text(style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold
+                        ), "Nom : ${item["preset_name"]!}"),
+                        subtitle: Text(style: const TextStyle(
+                            fontSize: 12,
+                            fontStyle: FontStyle.italic
+                          ),
+                            "Crée par : ${item['created_by']}\nCrée le : ${item["created_at"]}\n"),
+                        onTap: () {
+                          /*Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => ItemPage(item: item)));*/
+                        }
+                    )
+                ]
+              ),
+            ),
+            floatingActionButton: FloatingActionButton(
+                    onPressed: () => {},
+              tooltip: 'Créer un nouveau preset',
+              child: const Icon(Icons.add),
+    )
+        )
+    );
+  }
+}
+
+
+class ForumPage extends StatelessWidget {
+  const ForumPage({super.key});
+  static const pageItems = [
+    {"Dashboard": {
+      "icon": Icon(Icons.home),
+      "page": HomePage()
+    }
+    },
+    {"Presets": {
+      "icon": Icon(Icons.precision_manufacturing),
+      "page": PresetsPage()
+    }
+    },
+    {"Forum": {
+      "icon": Icon(Icons.forum),
+      "page": ForumPage()
+    }
+    },
+    {"Mon compte": {
+      "icon": Icon(Icons.person),
+      "page": AccountPage()
+    }}
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> menuTile = [];
+    for (final item in pageItems) {
+      var key = item.keys.first;
+      var icon = item.values.first["icon"];
+      Widget page = item.values.first["page"] as Widget;
+      menuTile.add(ListTile(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+        tileColor: Colors.grey[300],
+        leading: icon,
+        title: Text(key, textAlign: TextAlign.right),
+        onTap: () {
+          if (page.runtimeType.toString() == toString()) {
+            Navigator.pop(context);
+          }
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => page));
+        },
+      )
+      );
+    }
+    return MaterialApp(
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+            useMaterial3: true
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('PlantPilot'),
+              centerTitle: true,
+              backgroundColor: Colors.blue,
+            ),
+            drawer: Drawer(
+              child: ListView(
+                children: <Widget>[
+                  const DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                    child: Text('Menu'),
+                  ),
+                ] + menuTile,
+              ),
+            ),
+            body: SingleChildScrollView(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    const Center(child: Row(children: [Icon(Icons.arrow_downward), Text("Les topics"), Icon(Icons.arrow_downward)])),
+                    for (final item in data.topics)
+                      ListTile(
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+                          tileColor: Colors.grey[300],
+                          leading: const Icon(Icons.topic),
+                          title: Text(style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold
+                          ), "Sujet : ${item["topic_name"]!}"
+                          ),
+                          subtitle: Text(style: const TextStyle(
+                              fontSize: 12,
+                              fontStyle: FontStyle.italic
+                          ),
+                              "Crée par : ${item['created_by']}\nCrée le : ${item["created_at"]}\nDernier message par : ${item["last_message_by"]}\nDernier message le : ${item["last_message_at"]}\nNombre de message(s) : ${item["total_messages"]}"),
+                          onTap: () {
+                            /*Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => ItemPage(item: item)));*/
+                          }
+                      )
+                  ]
+              ),
+            ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => {},
+            tooltip: 'Créer un nouveau preset',
+            child: const Icon(Icons.add),
+          )
+            )
+    );
+  }
+}
+
+class AccountPage extends StatelessWidget {
+  const AccountPage({super.key});
+  static const pageItems = [
+    {"Dashboard": {
+      "icon": Icon(Icons.home),
+      "page": HomePage()
+    }
+    },
+    {"Presets": {
+      "icon": Icon(Icons.precision_manufacturing),
+      "page": PresetsPage()
+    }
+    },
+    {"Forum": {
+      "icon": Icon(Icons.forum),
+      "page": ForumPage()
+    }
+    },
+    {"Mon compte": {
+      "icon": Icon(Icons.person),
+      "page": AccountPage()
+    }}
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> menuTile = [];
+    for (final item in pageItems) {
+      var key = item.keys.first;
+      var icon = item.values.first["icon"];
+      Widget page = item.values.first["page"] as Widget;
+      menuTile.add(ListTile(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+        tileColor: Colors.grey[300],
+        leading: icon,
+        title: Text(key, textAlign: TextAlign.right),
+        onTap: () {
+          if (page.runtimeType.toString() == toString()) {
+            Navigator.pop(context);
+          }
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => page));
+        },
+      )
+      );
+    }
+    return MaterialApp(
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+            useMaterial3: true
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('PlantPilot'),
+              centerTitle: true,
+              backgroundColor: Colors.blue,
+            ),
+            drawer: Drawer(
+              child: ListView(
+                children: <Widget>[
+                  const DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                    child: Text('Menu'),
+                  ),
+                ] + menuTile,
+              ),
+            ),
+            body: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(this.runtimeType.toString())
+                ],
+              ),
+            )
+        )
     );
   }
 }
